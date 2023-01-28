2c9e80f98b9c3e3f59c751e3616d7c06e3867512
JangWoojun <woojun0107@naver.com>
2023-01-29 02:08:01 +0900

    5. 코드 UI MVC 패턴

--- Test/ThirdViewController.swift
+++ Test/ThirdViewController.swift
@@ -1,17 +1,5 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
-    @IBOutlet weak var mainLabel: UILabel!
-    
-    var someString: String?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        mainLabel.text = someString
-    }
-    
-    @IBAction func backButtonTapeed(_ sender: UIButton) {
-        dismiss(animated: true)
-    }
     
 }
