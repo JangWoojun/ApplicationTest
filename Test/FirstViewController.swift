52516abcb28bdb90077082016e97b5fe7a3d7e2d
JangWoojun <woojun0107@naver.com>
2023-01-30 09:14:54 +0900

    8. 네비게이션바 + 탭바 구현 (메모리 효율적)

--- Test/FirstViewController.swift
+++ Test/FirstViewController.swift
@@ -9,30 +9,60 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    
+    // 로그인 여부에 관련된 참/거짓 저장하는 속성
+    var isLoggedIn = false
+    
+    //var navigationController: UINavigationController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         makeUI()
+        
+        // ⭐️ 로그인이 되어있지 않다면 로그인화면 띄우기
+        // =====> viewDidAppear가 더 정확한 시점 (뷰가 뜬 다음으로 코드 옮기기) ⭐️⭐️⭐️
+        
+//        if !isLoggedIn {
+//            let vc = LoginViewController()
+//            vc.modalPresentationStyle = .fullScreen
+//            present(vc, animated: false, completion: nil)
+//        }
+
     }
     
-    // 네비게이션 바를 코드로 설정하기
+    // 다음화면을 띄우는 더 정확한 시점 ⭐️⭐️⭐️
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // ⭐️ 로그인이 되어있지 않다면 로그인화면 띄우기
+        if !isLoggedIn {
+            let vc = ViewController()
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: false, completion: nil)
+        }
+        
+    }
+    
+    
     func makeUI() {
         view.backgroundColor = .gray
         
-        let navigationBarAppearance = UINavigationBarAppearance()
-        navigationBarAppearance.configureWithOpaqueBackground()
-        navigationController?.navigationBar.standardAppearance = navigationBarAppearance
-        navigationController?.navigationBar.scrollEdgeAppearance = navigationBarAppearance
-        navigationController?.navigationBar.tintColor = .blue
-
-        navigationItem.scrollEdgeAppearance = navigationBarAppearance
-        navigationItem.standardAppearance = navigationBarAppearance
-        navigationItem.compactAppearance = navigationBarAppearance
-
-        navigationController?.setNeedsStatusBarAppearanceUpdate()
+        // (네비게이션바 설정관련) iOS버전 업데이트 되면서 바뀐 설정⭐️⭐️⭐️
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()  // 불투명으로
+        //appearance.backgroundColor = .brown     // 색상설정
         
-        navigationController?.navigationBar.isTranslucent = false
-        //navigationController?.navigationBar.backgroundColor = .white
+        //appearance.configureWithTransparentBackground()  // 투명으로
+        
+        navigationController?.navigationBar.tintColor = .blue
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
         title = "Main"
+        
     }
+    
     
 }
