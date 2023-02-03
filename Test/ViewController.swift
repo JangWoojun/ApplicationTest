c225ae356b895bf41535e735a1a3976c7740a433
JangWoojun <woojun0107@naver.com>
2023-02-03 10:20:24 +0900

    9. 테이블뷰 + 화면이동 (코드)

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -1,51 +1,88 @@
 import UIKit
 
 final class ViewController: UIViewController {
-        
-    // 로그인 버튼
-    private lazy var loginButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.backgroundColor = .blue
-        button.setTitle("Login", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-        return button
-    }()
 
-    // 굳이 뷰를 따로 만들지 않았음 (MVC패턴으로 만들지 않았음)
+    private let tableView = UITableView()
+    
+    var moviesArray: [Movie] = []
+    
+    var movieDataManager = DataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        makeUI()
+        
+        view.backgroundColor = .white
+        setupNaviBar()
+        setupTableView()
+        setupDatas()
+        setupTableViewConstraints()
+    }
+    
+    func setupNaviBar() {
+        title = "영화목록"
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .systemBlue
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
 
-    // UI설정하는 코드, 버튼의 오토레이아웃
-    func makeUI() {
-        view.backgroundColor = #colorLiteral(red: 0.8321695924, green: 0.985483706, blue: 0.4733308554, alpha: 1)
-        view.addSubview(loginButton)
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 120
+        
+        tableView.register(MyTableViewCell.self, forCellReuseIdentifier: "MovieCell")
+    }
+    
+    func setupDatas() {
+        movieDataManager.makeMovieData()
+        moviesArray = movieDataManager.getMovieData()
+    }
+    
+    func setupTableViewConstraints() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            loginButton.widthAnchor.constraint(equalToConstant: 120),
-            loginButton.heightAnchor.constraint(equalToConstant: 45),
-            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
         ])
-        
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(#function)
+        return moviesArray.count
     }
     
-    // 버튼 누르면 동작하는 코드 ===> 로그인하면, 디스미스 (탭바가 더 아래에 깔려있음)
-    @objc func loginButtonTapped() {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(#function)
         
-        // 전화면의 isLoggedIn속성에 접근하기 ⭐️
-//        if let presentingVC = presentingViewController { // 옵셔널 바인딩
-//            let tabBarCon = presentingVC as! UITabBarController   // 탭바에 접근하기
-//            let nav = tabBarCon.viewControllers?[0] as! UINavigationController  // 네비게이션바에 접근하기
-//            let firstVC = nav.viewControllers[0] as! FirstViewController  // FirstVC에 접근하기
-//            firstVC.isLoggedIn.toggle()  // 로그인 되었다고 상태 변화시키기 (실제 앱에서 이렇게 구현할 일은 없음)
-//        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MyTableViewCell
         
-        dismiss(animated: true, completion: nil)
+        cell.mainImageView.image = moviesArray[indexPath.row].movieImage
+        cell.movieNameLabel.text = moviesArray[indexPath.row].movieName
+        cell.descriptionLabel.text = moviesArray[indexPath.row].movieDescription
+        cell.selectionStyle = .none
+        
+        return cell
     }
+}
+
+extension ViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailVC = DetailViewController()
+        detailVC.movieData = moviesArray[indexPath.row]
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
 }
